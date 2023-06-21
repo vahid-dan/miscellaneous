1eed6be055e3b12cba673b69926846b15c71e482
vahid-dan <v.daneshmand@gmail.com>
2023-06-21 16:23:41 -0400

    Add git pull to initializer

--- gateways/system-setup/initializer.sh
+++ gateways/system-setup/initializer.sh
@@ -33,7 +33,7 @@ if [ ! -L $general_data_dir/$general_datalogger_data_dir ]; then
   echo "Creating datalogger directory symbolic link"
   ln -s $general_data_dir/$general_git_data_branch $general_data_dir/$general_datalogger_data_dir
 else
-  echo "Datalogger directory already exists"
+  echo "Datalogger directory already exists."
 fi
 
 # Clone the latest commit from the data branch if the directory does not exist
@@ -41,7 +41,9 @@ if [ ! -d "$general_data_dir_path" ]; then
   echo "Cloning data branch"
   git clone --depth 1 --branch $general_git_data_branch $general_git_repo $general_data_dir_path
 else
-  echo "Data directory already exists"
+  echo "Data directory already exists. Pulling the latest version ..."
+  cd $general_data_dir_path
+  git pull
 fi
 
 # Clone the latest commit from the logs branch if the directory does not exist
@@ -49,7 +51,9 @@ if [ ! -d "$general_logs_dir_path" ]; then
   echo "Cloning logs branch"
   git clone --depth 1 --branch $general_git_logs_branch $general_git_repo $general_logs_dir_path
 else
-  echo "Logs directory already exists"
+  echo "Logs directory already exists. Pulling the latest version ..."
+  cd $general_logs_dir_path
+  git pull
 fi
 
 # Clone the latest commit from the startup branch if the directory does not exist
@@ -57,5 +61,7 @@ if [ ! -d "$general_apps_dir/$startup_notifier_local_repo_dir" ]; then
   echo "Cloning startup notifier branch"
   git clone --depth 1 --branch $startup_notifier_git_branch $startup_notifier_git_repo $general_apps_dir/$startup_notifier_local_repo_dir
 else
-  echo "Startup notifier directory already exists"
+  echo "Startup notifier directory already exists. Pulling the latest version ..."
+  cd $general_apps_dir/$startup_notifier_local_repo_dir
+  git pull
 fi
