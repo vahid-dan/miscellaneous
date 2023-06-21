35c5701e25805727ab9eae57e36baa716aea65d6
vahid-dan <v.daneshmand@gmail.com>
2023-06-21 12:38:29 -0400

    Fix log path

--- gateways/git-maintenance/git-push.sh
+++ gateways/git-maintenance/git-push.sh
@@ -23,7 +23,7 @@ timestamp=$(date +"%D %T %Z")
 
 echo -e "\n############################ $general_gateway_name - $timestamp ############################\n" 2>&1 | tee -a $git_push_log_file_path
 
-echo -e "Data:\n" 2>&1 | tee -a $general_git_push_log_file_path
+echo -e "Data:\n" 2>&1 | tee -a $git_push_log_file_path
 cd $general_data_dir/$general_git_data_branch
 git add . 2>&1 | tee -a $git_push_log_file_path
 git commit -m "$timestamp: Git Backup" 2>&1 | tee -a $git_push_log_file_path
