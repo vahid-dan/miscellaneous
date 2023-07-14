3b84091896a7f11d4815194ac33834096132e9b9
vahid-dan <v.daneshmand@gmail.com>
2023-07-14 14:25:01 -0400

    Use tcpdump -w

--- gateways/system-monitors/network-interface-monitor.sh
+++ gateways/system-monitors/network-interface-monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Network Interface Monitor Script
-# Executd from the Gateways
+# Executed from the Gateways
 # Runs tcpdump to monitor a network interface
 # Usage: If required, run after reboot.
 
@@ -14,6 +14,7 @@ general_gateway_name=$(yq e '.general.gateway_name' $config_file)
 general_data_dir=$(yq e '.general.data_dir' $config_file)
 general_git_logs_branch=$(yq e '.general.git_logs_branch' $config_file)
 network_interface_monitor_enabled=$(yq e '.network_interface_monitor.enabled' $config_file)
+network_interface_monitor_log_rotation_interval=$(yq e '.network_interface_monitor.log_rotation_interval' $config_file)
 
 # Body of the script
 
@@ -36,9 +37,29 @@ for interface in $interfaces; do
       sleep 1  # Wait for 1 second before checking again
     done
 
-    # Run tcpdump on the interface
-    sudo tcpdump -i $interface_name -l -n | while read line; do
-      echo "$(date "+%Y-%m-%d") $line" | tee -a $interface_log_file_path
+    # Run tcpdump on the interface and write output to the file
+    sudo tcpdump -i $interface_name -G $network_interface_monitor_log_rotation_interval -w "$interface_log_file_path"_%Y-%m-%d_%H:%M:%S.pcap &
+    pid=$!
+
+    # Wait for tcpdump to stop, or for the interface to go down
+    while kill -0 $pid && ip link show $interface_name up &>/dev/null; do
+      sleep 1  # Wait for 1 second before checking again
+    done
+
+    # If tcpdump is still running, stop it
+    if kill -0 $pid; then
+      sudo kill $pid
+      wait $pid
+    fi
+
+    # If the interface is still up, start tcpdump again
+    if ip link show $interface_name up &>/dev/null; then
+      continue
+    fi
+
+    # Wait for the interface to come back up before starting tcpdump again
+    while ! ip link show $interface_name up &>/dev/null; do
+      sleep 1  # Wait for 1 second before checking again
     done
   done &
 done
