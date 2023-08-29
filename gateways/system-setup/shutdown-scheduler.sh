3a3233f423f3736fd26e9a6170d21bb44ade9264
vahid-dan <v.daneshmand@gmail.com>
2023-08-28 23:01:36 -0500

    Rename scheduler

---
+++ gateways/system-setup/shutdown-scheduler.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Shutdown Scheduler Module
+# Executd from the Gateways
+# Shuts down the system according to the maintenance mode and power mode
+# Usage: Run after reboot.
+
+########## HEADER ##########
+
+module_name=shutdown_scheduler
+
+# Load utility functions and configurations for gateways
+source /home/ubuntu/miscellaneous/gateways/base/utils.sh
+
+# Check if the module is enabled
+check_if_enabled "$module_name"
+
+# Redirect all output of this module to log_to_file function
+exec > >(while IFS= read -r line; do log_to_file "$module_name" "$line"; echo "$line"; done) 2>&1
+
+echo "########## START ##########"
+
+##########  BODY  ##########
+
+# Check the power mode and schedule the shutdown accordingly
+if [ "$general_gateway_power_mode" = "ac" ]; then
+    echo "The system is in AC power mode. Scheduling shutdown at $shutdown_scheduler_shutdown_time."
+    # Schedule shutdown at the given time
+    sudo shutdown -h "$shutdown_scheduler_shutdown_time"
+elif [ "$general_gateway_power_mode" = "battery" ]; then
+    echo "The system is in battery power mode. Scheduling shutdown in $shutdown_scheduler_post_reboot_delay_minutes minutes after reboot."
+    # Schedule shutdown after the given delay
+    sudo shutdown -h +$shutdown_scheduler_post_reboot_delay_minutes
+else
+    echo "Unknown power mode: $general_gateway_power_mode"
+    exit 1
+fi
+
+########## FOOTER ##########
+
+echo "##########  END  ##########"
+
+# Close stdout and stderr
+exec >&- 2>&-
+# Wait for all background processes to complete
+wait
