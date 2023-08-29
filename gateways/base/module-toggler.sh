06a32a6caa7a6b386bb56f5a61870b347e1a8458
vahid-dan <v.daneshmand@gmail.com>
2023-08-28 23:08:06 -0500

    Add module toggler

--- gateways/base/module-toggler.sh
+++ gateways/base/module-toggler.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Network Interface Monitor Module
+# Module Toggler
 # Executd from the Gateways
-# Monitors network activity on specific interface(s)
+# Report and update status of modules
 # Usage: Run when needed
-
 ########## HEADER ##########
 
 module_name=general # status_update is a part of general
@@ -19,72 +18,84 @@ echo "########## START ##########"
 
 ##########  BODY  ##########
 
+# Set up some colors for user-friendly display
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Function to get all modules without the "general" module
+get_filtered_modules() {
+    local modules=($(yq eval 'keys | .[]' $config_file))
+    local filtered_modules=()
+    for module in "${modules[@]}"; do
+        if [ "$module" != "general" ]; then
+            filtered_modules+=("$module")
+        fi
+    done
+    echo "${filtered_modules[@]}"
+}
+
+# Using the above function to get the filtered list
+filtered_modules=($(get_filtered_modules))
+
 # Function to display modules and their status
 display_modules_status() {
     echo "Modules and their current status:"
-    modules=($(yq eval 'keys | .[]' $config_file))
-    for idx in "${!modules[@]}"; do
-        if [ "${modules[$idx]}" == "general" ]; then
-            status=$(yq e ".general.gateway_maintenance_mode" $config_file)
-            echo "$((idx+1)). ${modules[$idx]} (maintenance_mode): $status"
-        else
-            status=$(yq e ".${modules[$idx]}.enabled" $config_file)
-            echo "$((idx+1)). ${modules[$idx]}: $status"
-        fi
+    max_index_length=${#filtered_modules[@]}
+    for idx in "${!filtered_modules[@]}"; do
+        formatted_idx=$(printf "%${max_index_length}s" "$((idx+1))")
+        status=$(yq e ".${filtered_modules[$idx]}.is_enabled" $config_file)
+        status_text_colored=$( [ "$status" == "true" ] && echo "${GREEN}[+] enabled${NC}" || echo "${RED}[-] disabled${NC}" )
+        echo -e "$formatted_idx. ${filtered_modules[$idx]}: $status_text_colored"
     done
 }
 
 # Function to toggle the status of a module
 toggle_module_status() {
-    local opt="$1"
+    local opt_idx="$1"
+    local opt="${filtered_modules[$opt_idx]}"
     local current_status
-
-    if [ "$opt" == "general" ]; then
-        current_status=$(get_config_value "general" "gateway_maintenance_mode")
-        if [ "$current_status" == "true" ]; then
-            yq eval ".${opt}.gateway_maintenance_mode = false" -i $config_file
-            echo "Set ${opt}'s gateway_maintenance_mode to false."
-        else
-            yq eval ".${opt}.gateway_maintenance_mode = true" -i $config_file
-            echo "Set ${opt}'s gateway_maintenance_mode to true."
-        fi
+    current_status=$(get_config_value "$opt" "is_enabled")
+    if [ "$current_status" == "true" ]; then
+        yq eval ".${opt}.is_enabled = false" -i $config_file
+        echo
+        echo "${opt} disabled."
     else
-        current_status=$(get_config_value "$opt" "enabled")
-        if [ "$current_status" == "true" ]; then
-            yq eval ".${opt}.enabled = false" -i $config_file
-            echo "Set ${opt} to false."
-        else
-            yq eval ".${opt}.enabled = true" -i $config_file
-            echo "Set ${opt} to true."
-        fi
+        yq eval ".${opt}.is_enabled = true" -i $config_file
+        echo
+        echo "${opt} enabled."
     fi
 }
 
 # Display modules and their status
+echo
 display_modules_status
+echo
 
 # Get user selection
-echo "Select modules to toggle its status (e.g., 1 3 6) or enter 'q' to exit without changes:"
+echo -e "Choose one or more modules (e.g., 1, 3, 6) to toggle their status (enable/disable), or type 'q' to exit without making any changes:"
 read -a selections
 
 for selected in "${selections[@]}"; do
     # Exit if user chooses the exit option
     if [ "$selected" == "q" ]; then
+        echo
         echo "Exiting without changes."
-    
     # Validate user input
-    elif [ "$selected" -lt 1 ] || [ "$selected" -gt "${#modules[@]}" ]; then
+    elif [ "$selected" -lt 1 ] || [ "$selected" -gt "${#filtered_modules[@]}" ]; then
+        echo
         echo "Invalid selection: $selected. Exiting without changes."
-    
     else
         idx=$((selected-1))
-        toggle_module_status "${modules[$idx]}"
+        toggle_module_status "$idx"
     fi
 done
 
 # After toggling the status, display and log the final status
+echo
 echo "Final Status:"
 display_modules_status 2>&1 | tee $general_data_dir/$general_git_logs_branch/$general_module_toggler_log_file
+echo
 
 ########## FOOTER ##########
 
